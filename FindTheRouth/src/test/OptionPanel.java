2eac4c8ae990d9526e522c4902b9f09417f3f8cd
Maria <Maria@192.168.1.149>
2016-04-23 21:14:03 +0200

    Test Commit: Option Panel Added

---
+++ FindTheRouth/src/test/OptionPanel.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import javax.swing.*;
+import java.util.*;
+
+/**
+ *
+ * @author Maria
+ */
+public class OptionPanel extends JFrame {
+    public OptionPanel(){
+        setTitle("Stores Orders and Road Info");
+        JLabel l1=new JLabel("Stores Orders:");
+        JLabel l2=new JLabel("Store 1 wants(S1):");
+    }
+}
